47b6147c901b2df37c703ddb89cfd2bf4a46a80a
Zulfikar Idris <zulfikarfull@gmail.com>
2017-01-05 14:40:20 +0700

    Set Servlet 3.X Java Configuration

--- src/main/java/com/kofera/app/web/config/AppInitializer.java
+++ src/main/java/com/kofera/app/web/config/AppInitializer.java
@@ -2,8 +2,11 @@ package com.kofera.app.web.config;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 /**
  * This App Initializer for Spring MVC (replace web.xml)
@@ -15,7 +18,14 @@ public class AppInitializer implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext container) throws ServletException {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(AppConfig.class);
+		context.setServletContext(container);
 		
+		ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(context));
+		
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
 	}
 
 }
