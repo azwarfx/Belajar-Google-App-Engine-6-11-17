b003a59dd2ff59af16ab54ec695d09d10d03494b
Zulfikar Idris <zulfikarfull@gmail.com>
2017-01-05 14:53:32 +0700

    Create the View and Stylesheet

--- src/main/java/com/kofera/app/web/controllers/HelloController.java
+++ src/main/java/com/kofera/app/web/controllers/HelloController.java
@@ -2,9 +2,11 @@ package com.kofera.app.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+	@GetMapping("/hello")
 	public String hello(Model model) {
 		
 		model.addAttribute("name", "John Dae");
