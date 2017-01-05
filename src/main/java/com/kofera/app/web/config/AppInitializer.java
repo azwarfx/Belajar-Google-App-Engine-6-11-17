fdc9db3869e9ceca806a9bcaf95589469363743a
Zulfikar Idris <zulfikarfull@gmail.com>
2017-01-05 14:31:38 +0700

    Set configuration for spring mvc

---
+++ src/main/java/com/kofera/app/web/config/AppInitializer.java
@@ -0,0 +1,21 @@
+package com.kofera.app.web.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+/**
+ * This App Initializer for Spring MVC (replace web.xml)
+ * 
+ * @author zulfikar@kofera.com
+ * @version 1.0
+ */
+public class AppInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext container) throws ServletException {
+		
+	}
+
+}
