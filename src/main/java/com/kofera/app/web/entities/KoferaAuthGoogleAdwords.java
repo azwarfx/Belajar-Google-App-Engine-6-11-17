f1d3479e95f8e1c196f63ce411f65479e4f9be61
fitribibi <fitri.b.suryani@gmail.com>
2017-01-05 14:39:54 +0700

    complete entity for kofera auth google adwords

---
+++ src/main/java/com/kofera/app/web/entities/KoferaAuthGoogleAdwords.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2017 Kofera Technology Inc. All Rights Reserved.
+ */
+package com.kofera.app.web.entities;
+
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Parent;
+
+/**
+ * This @Entity for kofera auth google adwords
+ * 
+ * @author fitri@kofera.com
+ * @version 1.0
+ */
+
+@Entity
+public class KoferaAuthGoogleAdwords extends KoferaModel {
+
+	@Parent
+	Key<KoferaAccount> koferaId;
+	String refresh_token;
+	String access_token;
+
+	/**
+	 * A convenience constructor
+	 */
+	public KoferaAuthGoogleAdwords(Long koferaId, String refresh_token, String access_token) {
+		super();
+		this.koferaId = Key.create(KoferaAccount.class, koferaId);
+		this.refresh_token = refresh_token;
+		this.access_token = access_token;
+	}
+
+	/**
+	 * @return the koferaId
+	 */
+	public Key<KoferaAccount> getKoferaId() {
+		return koferaId;
+	}
+
+	/**
+	 * @param set
+	 *            koferaId to this koferaId
+	 */
+	public void setKoferaId(Key<KoferaAccount> koferaId) {
+		this.koferaId = koferaId;
+	}
+
+	/**
+	 * @return the refresh_token
+	 */
+	public String getRefresh_token() {
+		return refresh_token;
+	}
+
+	/**
+	 * @param set
+	 *            refresh_token to this refresh_token
+	 */
+	public void setRefresh_token(String refresh_token) {
+		this.refresh_token = refresh_token;
+	}
+
+	/**
+	 * @return the access_token
+	 */
+	public String getAccess_token() {
+		return access_token;
+	}
+
+	/**
+	 * @param set
+	 *            access_token to this access_token
+	 */
+	public void setAccess_token(String access_token) {
+		this.access_token = access_token;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("{id: ").append(id).append(", created: ").append(createdAt).append(", koferaId:")
+				.append(koferaId).append(", refresh_token: ").append(refresh_token).append(", access_token: ")
+				.append(access_token).append("}");
+		return builder.toString();
+	}
+
+}
